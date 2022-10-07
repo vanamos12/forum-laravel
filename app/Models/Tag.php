6240e20639fbc2009e2b0ff250f42782317b1a41
vanamos12 <pokatchoneng@yahoo.fr>
2022-10-07 10:53:42 +0100

    Part 12 Tag model, Taggables, Migration and Trait Setup

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Traits\HasTags;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Tag extends Model
 {
     use HasFactory;
+
+    protected $table = 'tags';
+
+    protected $fillable = [
+        'name',
+        'slug'
+    ];
+
+    public function threads():MorphToMany{
+        return $this->morphedByMany(Thread::class, 'taggable');
+    }
 }
