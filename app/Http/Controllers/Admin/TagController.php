2d678714221f117c6c476859adc944ccb2a87483
vanamos12 <pokatchoneng@yahoo.fr>
2022-10-28 10:13:04 +0100

    Part 13 Tag Seeder, Controller and Routes

---
+++ app/Http/Controllers/Admin/TagController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Tag;
+use Illuminate\Http\Request;
+use App\Http\Middleware\IsAdmin;
+use App\Http\Controllers\Controller;
+use App\Http\Middleware\Authenticate;
+
+class TagController extends Controller
+{
+    public function __construct(){
+        return $this->middleware([IsAdmin::class, Authenticate::class]);
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return view('admin.tags.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('admin.tags.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Tag  $tag
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tag $tag)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Tag  $tag
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tag $tag)
+    {
+        //
+        return view('admin.tags.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Tag  $tag
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tag $tag)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Tag  $tag
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tag $tag)
+    {
+        //
+    }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -1,14 +1,19 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\TagController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\DashboardController;
-use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.'], function (){
     // Dashboard
     Route::get('/', [DashboardController::class, 'index'])->name('index');
 
-    // Categries
+    /**
+     * Name: Categories
+     * Url: /admin/categories
+     * Route: admin.categories.*
+     */
     Route::group(['prefix' => 'categories', 'as' => 'categories.'], function(){
         Route::get('/', [CategoryController::class, 'index'])->name('index');
         Route::get('create', [CategoryController::class, 'create'])->name('create');
@@ -16,5 +21,19 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.'], function (){
         Route::get('/edit/{category:slug}', [CategoryController::class, 'edit'])->name('edit');
         Route::put('/{category:slug}', [CategoryController::class, 'update'])->name('update');
         Route::delete('/{category:slug}', [CategoryController::class, 'destroy'])->name('delete');
+    });
+
+    /**
+     * Name: Tags
+     * Url: /admin/tags
+     * Route: admin.tags.*
+     */
+    Route::group(['prefix' => 'tags', 'as' => 'tags.'], function(){
+        Route::get('/', [TagController::class, 'index'])->name('index');
+        Route::get('create', [TagController::class, 'create'])->name('create');
+        Route::post('/', [TagController::class, 'store'])->name('store');
+        Route::get('/edit/{tag:slug}', [TagController::class, 'edit'])->name('edit');
+        Route::put('/{tag:slug}', [TagController::class, 'update'])->name('update');
+        Route::delete('/{tag:slug}', [TagController::class, 'destroy'])->name('delete');
     });
 });
