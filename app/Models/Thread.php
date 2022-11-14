5bde26e527731b8a6f2af4154c153cfb542ed67b
vanamos12 <pokatchoneng@yahoo.fr>
2022-11-14 16:30:02 +0100

    Part 18 Setup Thread model and HasAuthor

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\HasAuthor;
 use App\Traits\HasTags;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -11,8 +12,27 @@ class Thread extends Model
 {
     use HasFactory;
     use HasTags;
+    use HasAuthor;
+
+    protected $fillable = [
+        'subject',
+        'body',
+        'category_id',
+        'author_id',
+    ];
+
+    protected $with = [
+        'authorRelation',
+        'categories',
+        'tagsRelation'
+    ];
 
     public function category():BelongsTo{
         return $this->belongsTo(Category::class);
     } 
+
+    public function delete(){
+        $this->removeTags();
+        parent::delete();
+    }
 }
--- app/Traits/HasTags.php
+++ app/Traits/HasTags.php
@@ -17,6 +17,11 @@ trait HasTags{
         $this->unsetRelation('tagsRelation');
     }
 
+    public function removeTags(){
+        $this->tagsRelation()->detach();
+        $this->unsetRelation('tagsRelation');
+    }
+
     public function tagsRelation():MorphToMany{
         return $this->morphToMany(Tag::class, 'taggables')->withTimestamps();
     }
