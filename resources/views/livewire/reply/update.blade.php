e0fe284a1ebc64323b13f0ee515d147430bf6645
vanamos12 <pokatchoneng@yahoo.fr>
2022-12-03 11:36:30 +0100

    Part 46 Add Alpine.js to our view to allow us to edit our replies

--- resources/views/livewire/reply/update.blade.php
+++ resources/views/livewire/reply/update.blade.php
@@ -2,16 +2,35 @@
     <div x-data="{
         editReply: false,
         focus: function(){
-            const textInput = $this.$refs.textInput;
+            const textInput = this.$refs.textInput;
             textInput.focus();
+            console.log('focused', textInput)
         }
-    }">
+    }" x-cloak>
         {{-- Success is as dangerous as failure. --}}
-        {{ $replyOrigBody }}
+        <div x-show="!editReply" class="relative">
+            {{ $replyOrigBody }}
+            <x-links.secondary x-on:click="editReply = true; $nextTick(() => {focus()});" class="absolute cursor-pointer top-2 right-2">
+                {{ __('Edit') }}
+            </x-links.secondary>
+        </div>
 
-        <div>   
+        <div x-show="editReply">   
             <form wire:submit.prevent="updateReply">
-                <button>Save</button>
+                <input 
+                    class="w-full bg-gray-100 border-none shadow-inner focus:ring-blue-500"
+                    type="text" 
+                    name="replyNewBody" 
+                    wire:model.lazy="replyNewBody" 
+                    x-ref="textInput"
+                    x-on:keydown.enter="editReply = false"
+                    x-on:keydown.escape="editReply = false"
+                    />
+                <div class="mt-2 space-x-3 text-xs">
+                    <button type="button" class="text-green-400" x-on:click="editReply = false">Cancel</button>
+                    <button type="submit" class="text-red-400" x-on:click="editReply = false">Save</button>
+                </div>
+                
             </form>
         </div>
     </div>
