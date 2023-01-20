f5ebc30f34e808fe0d80408f0a136ca85c098054
vanamos12 <pokatchoneng@yahoo.fr>
2023-01-20 10:35:15 +0100

    Part 67 Subscribe: Setup Models and Interface

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -2,10 +2,33 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Subscription extends Model
 {
     use HasFactory;
+
+    Const TABLE = 'subscriptions';
+
+    protected $table = self::TABLE;
+
+    public function user():User{
+        return $this->userRelation;
+    }
+
+    public function userRelation(): BelongsTo{
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function subscriptionAble(): SubscriptionAble{
+        return $this->subscriptionAbleRelation;
+    }
+
+    public function subscriptionAbleRelation(): MorphTo{
+        return $this->morphTo('subscriptionAbleRelation', 'subscriptionable_type', 'subscriptionable_id');
+    }
 }
