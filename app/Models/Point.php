d7ed927fcc94625b045d684b4087c461ca7af0ec
vanamos12 <pokatchoneng@yahoo.fr>
2023-02-14 18:19:50 +0100

    Part 82 : Awards/Points to users: models setup

--- app/Models/Point.php
+++ app/Models/Point.php
@@ -4,8 +4,49 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Point extends Model
 {
     use HasFactory;
+
+    const TABLE = 'points';
+
+    protected $table = self::TABLE;
+
+    protected $fillable = [
+        'id',
+        'amount',
+        'message',
+        'current_points',
+    ];
+
+    public function pointable(): MorphTo{
+        return $this->morphTo();
+    }
+
+    public function getCurrentPoints(Model $pointable):Model{
+        $currentPoints = Point::where('pointable_id', $pointable->id())
+        ->where('pointable_type', $pointable->getMorphClass())
+        ->orderBy('created_at', 'desc')
+        ->pluck('current_points')
+        ->first();
+        if (!$currentPoints){
+            $currentPoints = 0;
+        }
+        return $currentPoints;
+    }
+
+    public function addAwards(Model $pointable, $amount, $message){
+        $award = new Static();
+        $award->amount = $amount;
+        $award->current_points = $this->getCurrentPoints($pointable) + $amount;
+        $award->message = $message;
+
+        $pointable->awards()->save();
+
+        return $award;
+
+    }
+
 }
--- app/Models/PointAble.php
+++ app/Models/PointAble.php
@@ -3,5 +3,9 @@
 namespace App\Models;
 
 interface PointAble{
-    
+    public function awards();
+
+    public function countAwards();
+
+    public function addPoints($amount, $message);
 }
